0f0f4241da10b095817b0ae63b15c3b4e49648c2
Jordan <jwald3@brockport.edu>
2025-04-12 10:48:45 -0400

    fixed test runner macro

--- TestRunner.h
+++ TestRunner.h
@@ -8,6 +8,8 @@ class TestRunner {
      static void runTest(const std::string& testName, std::function<bool()> test);
  };
  
+ // needed to add paranetheses around "expected" and "actual" because "==" comes after "<<", so the evaluation
+ // isn't performed by the time the "<<" resolves. Parentheses have precedence over the shift. 
  #define ASSERT_EQ(expected, actual)   \                                       
      ((expected) == (actual)) ? true : \
-     (std::cout << "Assertion failed: " << expected << " != " << actual << std::endl,  false)
+     (std::cout << "Assertion failed: " << (expected) << " != " << (actual) << std::endl,  false)
