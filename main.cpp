d8d25dc58060485c6a3fd2fe35bdd2b50d918daa
Jordan <jwald3@brockport.edu>
2025-04-13 21:30:13 -0400

    Enable test for Rational Large Numbers

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     TestRunner::runTest("Basic Rational Operations", testRationalBasicOperations);
     TestRunner::runTest("Rational Edge Cases", testRationalEdgeCases);
-    // TestRunner::runTest("Rational Large Numbers", testRationalLargeNumbers);
+    TestRunner::runTest("Rational Large Numbers", testRationalLargeNumbers);
     TestRunner::runTest("Rational String Conversion", testRationalStringConversion);
     TestRunner::runTest("Rational Comparison Operators", testRationalComparisonOperators);
     TestRunner::runTest("Rational Unary Operators", testRationalUnaryOperators);
